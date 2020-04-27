99952e07d3940ddf961acca2cbe84ece15fd00c6
Harry Shen <tn850826@icloud.com>
2020-04-28 01:33:32 +1000

    format the content of stock history table

--- src/Components/StockDetail.js
+++ src/Components/StockDetail.js
@@ -3,29 +3,127 @@ import '../App.css';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-import { useStockHistories } from '../stock_apis';
+import { getStockHistory } from '../stock_apis';
+import { Label, Col, Row } from 'reactstrap';
+import moment from 'moment';
+
+function Dropdown(props) {
+    const [innerHistories, setInnerHistories] = useState([]);
+    const [innerSelect, setInnerSelect] = useState("");
+
+    useEffect(() => {
+        setInnerHistories(props.histories);
+        if(innerSelect !== "") {
+            props.onSelectHistory(innerSelect);
+        }
+    }, [innerSelect, props]);
+
+    return (
+        <div className="hisotry-dropdown">
+            <Label>Search date from</Label>
+            <Row>
+                <Col xs="6" sm="4" />
+                <Col xs="6" sm="4">
+                    <select 
+                        className="custom-select"  
+                        onChange={e => {
+                        setInnerSelect(e.target.value);
+                        }}>
+                        <option value=""></option>
+                        {innerHistories.map(history => (
+                        <option key={innerHistories.indexOf(history)} value={history}>
+                            {history}
+                        </option>
+                        ))}
+                    </select>
+                </Col>
+                <Col sm="4" />
+            </Row>
+        </div>
+    );
+}
+
+function GridTable(props) {
+    const gridOptions = {
+        columnDefs: [
+            { 
+                headerName: "Date", 
+                field: "timestamp", 
+                width: 130,
+                cellRenderer: (data) => {
+                    return moment(data.createdAt).format('L');
+                }
+            },
+            { headerName: "Open", field: "open", width: 100},
+            { headerName: "High", field: "high", width: 100},
+            { headerName: "Low", field: "low", width: 100},
+            { headerName: "Close", field: "close", width: 100},
+            { headerName: "Volumes", field: "volumes", width: 100},
+        ]
+    };
+    
+    return (
+        <div 
+        className="ag-theme-balham table-container"
+        style={{
+            height: "300px",
+            width: "65vh",
+        }}
+        >
+          <AgGridReact 
+              rowData={props.histories}
+              pagination={true}
+              paginationAutoPageSize={true}
+              gridOptions={gridOptions}
+              reactNext={true}
+          />
+        </div> 
+    );
+}
 
 function StockDetail(props) {
     const symbol = props.location.search.substring(8);
-    const { loading, data, error } = useStockHistories(symbol);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [timestamp, setTimestamp] = useState("");
     const [histories, setHistories] = useState([]);
-    
+    const timestamps = histories.map(history => {
+        return moment(history.timestamp).format('L');
+    });
     useEffect(() => {
-        setHistories(data);
-    }, [data]);
-
+        getStockHistory(symbol, timestamp)
+            .then(histories => {
+                setHistories(histories);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(true);
+            });
+    }, [histories, timestamp]);
+    
+    
     if(loading) {
         return <div>Loading...</div>
     }
     if(error) {
         return <div>An error Occurred: {error.message}</div>
     }
-    
-    
 
     return (
         <div className="App">
-        <h1>Stock Detail Page</h1>
+            <div className="container">
+                <h1>Stock Detail Page</h1>
+                <Dropdown 
+                onSelectHistory={
+                    value => setTimestamp(moment(value).format().substring(0,10))
+                } 
+                histories={timestamps}/>
+            </div>
+                <Label>Showing stocks for {histories[0].name}</Label>
+            <div className="grid-table">
+                <GridTable histories={histories}/>
+            </div>
         </div>
     );
 }
--- src/stock_apis.js
+++ src/stock_apis.js
@@ -25,30 +25,30 @@ export function useAllStocks() {
     };
 }
 
-export function useStockHistories(symbol, from) {
-    const [loading, setLoading] = useState(true); 
-    const [data, setData] = useState([]); 
-    const [error, setError] = useState(null);
+// export function useStockHistories(symbol, from) {
+//     const [loading, setLoading] = useState(true); 
+//     const [data, setData] = useState([]); 
+//     const [error, setError] = useState(null);
     
-    useEffect(() => { 
-        getStockHistory(symbol, from)
-        .then( histories => {
-            setData(histories);
-            setLoading(false);
-        })
-          .catch((e) => { 
-            setError(e);
-            setLoading(false); 
-          }); 
-        },
-    // This blank array are the 'dependencies'
-    []);
-    return { 
-    loading,
-    data,
-    error, 
-    };
-}
+//     useEffect(() => { 
+//         getStockHistory(symbol, from)
+//         .then( histories => {
+//             setData(histories);
+//             setLoading(false);
+//         })
+//           .catch((e) => { 
+//             setError(e);
+//             setLoading(false); 
+//           }); 
+//         },
+//     // This blank array are the 'dependencies'
+//     [symbol, from]);
+//     return { 
+//     loading,
+//     data,
+//     error, 
+//     };
+// }
 
 function getStocks() {
     return fetch("http://131.181.190.87:3001/all")
@@ -63,7 +63,7 @@ function getStocks() {
         );
 }
 
-function getStockHistory(symbol, from="") {
+export function getStockHistory(symbol, from="") {
     const url = `http://131.181.190.87:3001/history?symbol=${symbol}` +
     (from? `&from=${from}`: "")
     return fetch(url)
